347d261e0551c08c91d8014718da8aeb2627b475
github username <github email address>
2021-04-22 21:52:22 -0400

    commit tree 036c744e22c67deccf3cdff5aeb6756549fd3606

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(addThemMfs,0)
+
+function addThemMfs(accm, amount) {
+    return accm + amount
+}
